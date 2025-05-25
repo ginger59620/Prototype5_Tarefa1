96a2c1ce6d8e65ad7b6494af2677fa4552b14711
ginger59620 <a59620@alunos.ipb.pt>
2025-05-25 17:27:50 +0100

    difficulty set

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -4,16 +4,27 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManager;
+
+    public int difficulty;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         button = GetComponent<Button>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        button.onClick.AddListener(SetDifficulty);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + "was clicked");
+        gameManager.StartGame(difficulty);
     }
 }
